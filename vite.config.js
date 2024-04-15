7a0c4ccc95b46d90bf08b5dee69dd43608ae3d52
Olajide Samuel <iamosharry@gmail.com>
2024-04-15 11:34:55 -0700

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/react_stripe_project",
+  base: "/stripe/",
   plugins: [react()],
 })
