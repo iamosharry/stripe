dee75803d5e67896353fcebba307e4ed3cc6638a
Olajide Samuel <iamosharry@gmail.com>
2024-04-18 14:11:24 -0700

    image deployement

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './Navbar.css'
-import logo from '../../assets/logo-white.svg'
+import logoWhite from '../../assets/logo-white.svg'
 
 const Navbar = () => {
   const [menu, setMenu] = useState('Contact')
@@ -12,7 +12,7 @@ const Navbar = () => {
   return (
     <div className='navbar'>
       <div className="navbar-container">
-        <img src={logo} alt="" />
+        <img src={logoWhite} alt="" />
         <ul>
           <li onMouseMove={()=>{setMenu('Products')}} className={menu === 'Products' ? 'active' : ''} >Products</li>
           <li onMouseMove={()=>{setMenu('Developers')}} className={menu == 'Developers' ? 'active' : ''}>Developers</li>
